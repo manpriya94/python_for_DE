e63b8afff9be7c598ef4311bc7f7bbf9734cf1d7
Priyanka Agrawal <agrawalpriyanka1612@gmail.com>
2025-06-01 09:49:56 +0530

    added assignments

---
+++ Day1/Q2.py
@@ -0,0 +1,9 @@
+""" 
+2- Odd or Even : Ask the user to enter a number. Print whether the number is even or odd. """
+
+number = input("Please enter a number")
+
+if (int(number) % 2) == 0:
+    print("number is even")
+else: 
+    print("number is odd")
